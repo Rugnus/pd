c442ffb9b8012b9a24ae33a6554e520dcb202f17
Gasanov Sungur <sungur.gasanov@mail.ru>
2020-06-30 15:46:53 +0300

    Added a search a temp by city

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,12 +1,9 @@
 import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 
-
-const apiKey = '0fef488560ab5706a17869e15ac347e3';
 
 
 class LoadingScreen extends StatefulWidget {
@@ -27,13 +24,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async{
 
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(locationWeather: weatherData ,
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,8 @@
+import 'package:clima/screens/city_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
 
@@ -29,6 +31,13 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI( dynamic weatherData) {
     setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        weatherIcon = 'Error';
+        temperatureMessage = 'Unable to get weather data';
+        cityName = '';
+        return;
+      }
       var condition = weatherData['weather'][0]['id'];
       weatherIcon = weather.getWeatherIcon(condition);
 
@@ -62,14 +71,29 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async{
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async{
+                      var typedName = await  Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context){
+                              return CityScreen();
+                          },),
+                      );
+                      if (typedName != null) {
+                        var weatherData = await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
