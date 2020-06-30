ac5051fb38be0d701d472105e71dfc0bb2a29f46
Gasanov Sungur <sungur.gasanov@mail.ru>
2020-06-30 14:04:12 +0300

    Getting a exact location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,8 @@ import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKey = '0fef488560ab5706a17869e15ac347e3';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,6 +12,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  double latitude;
+  double longitude;
 
   @override
   void initState() {
@@ -24,13 +29,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude =location.latitude;
+    longitude =location.longitude;
+
+    getData();
 
   }
 
   void getData() async{
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     if (response.statusCode == 200) {
         String data = response.body;
 
@@ -55,7 +62,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
